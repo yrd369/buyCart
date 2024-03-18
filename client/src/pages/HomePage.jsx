584f4623543e760749cfae977149aeef3ed49a3f
yrd369 <yogharajdhayalnamburajan@gmail.com>
2024-03-18 14:12:37 +0530

    improve: added context

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import Card from "../components/Card";
 import Form from "../components/Form";
+import { productContext } from "../hooks/productContext";
 
 const HomePage = () => {
   const [data, setData] = useState();
@@ -22,13 +23,15 @@ const HomePage = () => {
   return (
     <>
       <NavBar />
-      <div className="container mx-auto flex space-x-3">
-        {data &&
-          data.map((val) => {
-            return <Card value={val} key={val._id} />;
-          })}
+      <productContext.Provider value={data}>
+        <div className="container mx-auto flex space-x-3">
+          {data &&
+            data.map((val) => {
+              return <Card value={val} key={val._id} />;
+            })}
           <Form />
-      </div>
+        </div>
+      </productContext.Provider>
     </>
   );
 };
